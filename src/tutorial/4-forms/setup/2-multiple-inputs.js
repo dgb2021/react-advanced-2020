0c861c2f4c0a56126fa8d4ecf8a1d483fb5e78ae
Doug Barr <douglas.barr01@gmail.com>
2021-07-13 12:11:32 +0100

    multiple forms

--- src/tutorial/4-forms/setup/2-multiple-inputs.js
+++ src/tutorial/4-forms/setup/2-multiple-inputs.js
@@ -9,10 +9,20 @@ import React, { useState } from 'react';
 const ControlledInputs = () => {
   const [person, setPerson] = useState({ firstName: '', email: '', age: '' });
   const [people, setPeople] = useState([]);
+  
+  const handleChange = (e) => {
+    const name = e.target.name
+    const value = e.target.value
+    setPerson({ ...person, [name]: value});
 
-const handleChange = (e) => {}
+}
 const handleSubmit = (e)=> {
   e.preventDefault();
+  if (person.firstName && person.email && person.age) {
+    const newPerson = {...person, id: new Date().getTime().toString()}
+    setPeople([...people, newPerson]);
+    setPerson({firstName: '', email: '', age: ''})
+  }
 }
   
   return (
@@ -52,10 +62,11 @@ const handleSubmit = (e)=> {
           <button type='submit' onClick={handleSubmit}>add person</button>
         </form>
         {people.map((person, index) => {
-          const { id, firstName, email } = person;
+          const { id, firstName, email, age } = person;
           return (
             <div className='item' key={id}>
               <h4>{firstName}</h4>
+              <p>{age}</p>
               <p>{email}</p>
             </div>
           );
