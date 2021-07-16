7fb5c8d30774bb52a782b620920b22aa0e6c5cb8
Doug Barr <douglas.barr01@gmail.com>
2021-07-16 12:21:53 +0100

    move reduce to its own file

--- src/tutorial/6-useReducer/setup/index.js
+++ src/tutorial/6-useReducer/setup/index.js
@@ -1,28 +1,7 @@
 import React, { useState, useReducer } from 'react';
 import Modal from './Modal';
 import { data } from '../../../data';
-// reducer function
-
-const reducer = (state, action) => {
-  
-  if(action.type === 'ADD_ITEM') {
-    const newPeople = [...state.people, action.payload ]
-    return {
-      ...state,
-      people: newPeople,
-      isModalOpen: true,
-      modalContent: 'item added',
-    };
-  }
-  if (action.type === 'NO_VALUE') {
-    return {...state, isModalOpen: true, modalContent: 'please enter value' }
-  }
-
-  if (action.type === 'CLOSE_MODAL') {
-    return { ...state, isModalOpen: false }
-  }
-  throw new Error('no matching action type');
-};
+import {reducer} from './reducer';
 
 const defaultState = {
   people: [],
@@ -64,8 +43,11 @@ const Index = () => {
       </form>
       {state.people.map((person) => {
         return (
-          <div key={person.id}>
+          <div key={person.id} className='item'>
             <h4>{person.name}</h4>
+            <button onClick={() => dispatch ( { type: 'REMOVE_ITEM', payload:person.id })}>
+              remove
+            </button>
           </div>
         );
       })}
